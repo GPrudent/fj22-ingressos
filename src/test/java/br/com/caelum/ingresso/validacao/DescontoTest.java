be6c19962a5113a742ca26f61fe7f806167f6188
GPrudent <guilhermeprudente.18@gmail.com>
2017-07-19 16:42:15 -0300

    implementada logica de bloquear seleção de carrinhos ja selecionados

--- src/test/java/br/com/caelum/ingresso/validacao/DescontoTest.java
+++ src/test/java/br/com/caelum/ingresso/validacao/DescontoTest.java
@@ -10,9 +10,10 @@ import org.junit.Test;
 
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Ingresso;
+import br.com.caelum.ingresso.model.Lugar;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
-import br.com.caelum.ingresso.model.descontos.DescontoDeTrintaPorCentoParaBancos;
+import br.com.caelum.ingresso.model.TipoDeIngresso;
 import br.com.caelum.ingresso.model.descontos.DescontoEstudante;
 import br.com.caelum.ingresso.model.descontos.SemDesconto;
 
@@ -20,10 +21,11 @@ public class DescontoTest {
 	
 	@Test
 	public void deveConcederDescontoDe30PorCentoParaIngressosDeClientesDeBancos(){
+		Lugar lugar = new Lugar("A",1);
 		Sala sala = new Sala("Eldorado - IMAX",new BigDecimal("20.5"));
 		Filme filme = new Filme("Rogue One",Duration.ofMinutes(120),"sci-fi",new BigDecimal("12"));
 		Sessao sessao = new Sessao(LocalTime.now(),filme,sala);
-		Ingresso ingresso = new Ingresso(sessao,new DescontoDeTrintaPorCentoParaBancos());
+		Ingresso ingresso = new Ingresso(sessao,TipoDeIngresso.BANCO,lugar);
 		
 		BigDecimal precoEsperado = new BigDecimal("22.75");
 		
@@ -34,7 +36,8 @@ public class DescontoTest {
 		Sala sala = new Sala("Eldorado - IMAX",new BigDecimal("20.5"));
 		Filme filme = new Filme("Rogue One",Duration.ofMinutes(120),"sci-fi",new BigDecimal("12"));
 		Sessao sessao = new Sessao(LocalTime.now(),filme,sala);
-		Ingresso ingresso = new Ingresso(sessao,new DescontoEstudante());
+		Lugar lugar = new Lugar("A",1);
+		Ingresso ingresso = new Ingresso(sessao,TipoDeIngresso.ESTUDANTE,lugar);
 		
 		BigDecimal precoEsperado = new BigDecimal("16.25");
 		
@@ -46,7 +49,8 @@ public class DescontoTest {
 		Sala sala = new Sala("Eldorado - IMAX",new BigDecimal("20.5"));
 		Filme filme = new Filme("Rogue One",Duration.ofMinutes(120),"sci-fi",new BigDecimal("12"));
 		Sessao sessao = new Sessao(LocalTime.now(),filme,sala);
-		Ingresso ingresso = new Ingresso(sessao,new SemDesconto());
+		Lugar lugar = new Lugar("A",1);
+		Ingresso ingresso = new Ingresso(sessao,TipoDeIngresso.INTEIRO,lugar);
 		
 		BigDecimal precoEsperado = new BigDecimal("32.5");
 		
